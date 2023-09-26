7315f0c1ad087465c5ed5467c9aa70b153e5172b
Lucy Moore <lmoore36@unc.edu>
2023-09-26 15:12:28 -0400

    comp 210 upload

---
+++ lectures/Arrays.java
@@ -0,0 +1,10 @@
+package lectures;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int [] a = {1, 2, 3, 5};
+
+        int sum = 0; // = sum of a[i]
+        for (int i=0; i)
+    }
+}
---
+++ lectures/Example1.java
@@ -0,0 +1,22 @@
+package lectures;
+
+public class Example1 {
+    public static void main(String[] args){
+        // reference data types
+        byte by = 123;  by = 123;
+        short sh = 12345; sh = 12345;
+        int n = 1234567890; n: 1234567890;
+        long l = 1234567890; l: 1234567890;
+        float fl = 9.9f; fl: 9.9;
+        double d = 9.9; d: 9.9;
+        boolean b = true; b: true
+        char c = 'a'; c: 'a' 97
+
+        // reference data types
+        String s = "Hello"; s: "Hello"
+        String[] sA = {"Hi", "Bye"}; sA: ["Hi", "Bye"]
+        int[] nA = {1, 5, 20}; nA: [1, 5, 20]
+        char[] cA = {'a', 'b'}; cA: [a, b]
+        float[] fA = {2.5f, 3.2f}; fA: [2.5, 3.2]
+    }
+}
